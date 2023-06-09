408a43961afbf00a77a8495ae2b4bee53da39bc1
teddy cubaka <teddycubaka96@gmail.com>
2023-06-09 14:50:49 +0100

    delete : the product method

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Proprety, PropretySchema } from './schemas/proprety.schema';
-import { Product, ProductSchema } from 'src/schemas/product.schema';
-import { ProductService } from './services/product.service';
-import { ProductController } from './controllers/product.controller';
 import { PropretyController } from './controllers/proprety.controller';
 import { PropretyService } from './services/proprety.service';
 
@@ -14,10 +11,9 @@ import { PropretyService } from './services/proprety.service';
     MongooseModule.forRoot('mongodb://localhost:27017/zubu_v2'),
     MongooseModule.forFeature([
       { name: Proprety.name, schema: PropretySchema },
-      { name: Product.name, schema: ProductSchema },
     ]),
   ],
-  controllers: [AppController, ProductController, PropretyController],
-  providers: [AppService, ProductService, PropretyService],
+  controllers: [AppController, PropretyController],
+  providers: [AppService, PropretyService],
 })
 export class AppModule {}
