c5da6e618a6f08041005eeaf123a5fd34bfdcc34
teddy cubaka <teddycubaka96@gmail.com>
2023-07-03 17:54:40 +0100

    create : auth middleware"

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,16 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class authMiddleware implements NestMiddleware {
+  constructor(private jwtService: JwtService) {}
+  use(request: Request, res: Response, next: NextFunction) {
+    const decodedJwtAccessToken: any = this.jwtService.decode(
+      request.headers.authorization.split(' ')[1],
+    );
+    request.body.access = decodedJwtAccessToken;
+    console.log(decodedJwtAccessToken);
+    next();
+  }
+}
