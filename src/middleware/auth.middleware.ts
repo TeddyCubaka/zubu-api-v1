a032aa766a4eb2cc44fb11975f55fd0637e43353
teddy cubaka <teddycubaka96@gmail.com>
2023-07-04 16:58:00 +0100

    update : verify if token as already expired

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,16 +1,22 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class authMiddleware implements NestMiddleware {
   constructor(private jwtService: JwtService) {}
-  use(request: Request, res: Response, next: NextFunction) {
+
+  async use(request: Request, response: Response, next: NextFunction) {
     const decodedJwtAccessToken: any = this.jwtService.decode(
       request.headers.authorization.split(' ')[1],
     );
-    request.body.access = decodedJwtAccessToken;
-    console.log(process.env.JWT_SECRET_KEY);
-    next();
+    request.headers.authorization = decodedJwtAccessToken;
+
+    const myTime = new Date().getTime();
+    if (decodedJwtAccessToken.expireDate < myTime)
+      response
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'The token as expired' });
+    else next();
   }
 }
