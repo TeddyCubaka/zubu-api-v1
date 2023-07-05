a06fde22b9e29e4926f66a3b1071d9299a406c20
teddy cubaka <teddycubaka96@gmail.com>
2023-07-05 13:53:00 +0100

    update : middleware hardening

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,10 +1,14 @@
 import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request, Response, NextFunction } from 'express';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class authMiddleware implements NestMiddleware {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private usersService: UsersService,
+  ) {}
 
   async use(request: Request, response: Response, next: NextFunction) {
     const decodedJwtAccessToken: any = this.jwtService.decode(
@@ -13,10 +17,23 @@ export class authMiddleware implements NestMiddleware {
     request.headers.authorization = decodedJwtAccessToken;
 
     const myTime = new Date().getTime();
+
+    let isUserIdNoValid: boolean = false;
+    await this.usersService
+      .findOneById(decodedJwtAccessToken.sub)
+      .then((user) => {
+        if (user == null) isUserIdNoValid = true;
+      })
+      .catch(() => (isUserIdNoValid = true));
+
     if (decodedJwtAccessToken.expireDate < myTime)
       response
         .status(HttpStatus.UNAUTHORIZED)
-        .json({ message: 'The token as expired' });
+        .json({ message: 'The token has expired' });
+    else if (isUserIdNoValid)
+      response
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'Invalid token.' });
     else next();
   }
 }
