096983faabb55819692ef735004d867d04582da1
teddy cubaka <teddycubaka96@gmail.com>
2023-07-06 14:05:38 +0100

    style : lint the project

--- src/services/proprety.service.ts
+++ src/services/proprety.service.ts
@@ -63,16 +63,6 @@ export class PropretyService {
   }
 
   async updateAll(data: any): Promise<any> {
-    // Proprety.updateMany({}, [
-    //   {
-    //     $set: {
-    //       "rentalInformation.address": { $toLower: "$rentalInformation.address" },
-    //     },
-    //   },
-    // ])
-    //   .limit(20)
-    //   .then((data) => res.status(200).json(data))
-    //   .catch((err) => res.status(404).json(err));
     return this.PropretyModel.updateMany({}, data)
       .then((data) => data)
       .catch((err) => err);
