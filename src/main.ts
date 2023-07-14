d5891c9a1e37e66843138fb749a35b582de9d006
teddy cubaka <teddycubaka96@gmail.com>
2023-07-14 14:29:13 +0100

    refactor : review cors authorization

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,27 @@ async function bootstrap() {
   //   optionsSuccessStatus: 204,
   // });
   app.setGlobalPrefix('api/v1');
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
+  var whitelist = ['http://localhost:3000', 'https://www.website.com'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
 }
