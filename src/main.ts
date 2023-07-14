c47b1315e1962724984242736a78dc4c1e719bab
teddy cubaka <teddycubaka96@gmail.com>
2023-07-14 13:34:33 +0100

    refactor : review cors authorization

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
   app.setGlobalPrefix('api/v1');
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   await app.listen(process.env.PORT || 3000);
 }
 export default bootstrap();
