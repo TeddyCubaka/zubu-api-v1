3cccb908d617dabf35afd4f2731724e2fb7eabce
teddy cubaka <teddycubaka96@gmail.com>
2023-07-14 15:35:06 +0100

    refactor : review cors authorization

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: '*',
   });
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
@@ -20,4 +20,4 @@ export default bootstrap();
 //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
 //   next();
-// });
+// });  Content-Type, Accept
