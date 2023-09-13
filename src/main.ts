7101148c2c8ec6cf1247c7e9f4dfbfdf561c3bfb
teddy cubaka <teddycubaka96@gmail.com>
2023-09-13 13:11:36 +0100

    refactor : config vercel file to deployu to vercel

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,3 @@ async function bootstrap() {
 }
 export default bootstrap();
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-//   next();
-// });
